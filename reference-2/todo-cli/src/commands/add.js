202f17738a432c074f4917528f30a7e098ece893
youngbeen <pybju0@gmail.com>
2024-10-18 19:29:31 +0900

    gitignore추가

---
+++ reference-2/todo-cli/src/commands/add.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+import { saveTodos, loadTodos } from "../utils/fileHandler.js"
+import { v4 as uuid4 } from "uuid"
+import chalk from "chalk";
+
+export const addTodo = async () => { // 다른 모듈에서 사용할 수 있도록 내보내도록 수정
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "할 일의 제목을 입력하세요:",
+      validate: (input) => (input ? true : "제목을 입력해야 합니다."),
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "할 일의 설명을 입력하세요. (선택 사항):",
+    },
+  ]);
+
+  const todos = await loadTodos();
+
+  todos.push({
+    id: uuid4(),
+    title: answers.title,
+    description: answers.description || "",
+    completed: false,
+  });
+
+  await saveTodos(todos);
+
+  console.log(chalk.green("할 일이 성공적으로 추가되었습니다!"));
+}
